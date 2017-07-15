02ca14a8d63ea442032394f380fe03c9da5c0614
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2017-07-14 21:34:06 -0700

    Initial commit

---
+++ pytouch/pytouch.py
@@ -0,0 +1,13 @@
+import sys
+
+def main():
+    # sys.argv[0] is the path to the python interpreter
+    # sys.argv[1] should contain the name of the file we want to create
+    if len(sys.argv) != 3:
+        raise ValueError("Please provide the name of the file to create.")
+
+    filename = sys.argv[1]
+
+    # 'w+' erases the file if it exists and creates it if the file doesn't exist
+    with open(filename, 'w+'):
+        pass
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    # The name of our pip package
+    name='pytouch',
+    # The Python packages in this project
+    packages=[
+        # This is the `pytouch` folder that contains __init__.py and pytouch.py
+        'pytouch',
+    ],
+    version="0.0.1",
+    entry_points={
+        'console_scripts': [
+            # We use this line to map our `main()` method in pytouch.py
+            # to a shell command `pytouch`
+            'pytouch = pytouch.pytouch:main',
+        ],
+    },
+)
